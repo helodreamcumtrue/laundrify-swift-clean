1a44aeed84450ea03ca59dcac584728507b60be2
helodreamcumtrue <dreamycrafts56@gmail.com>
2025-06-25 00:01:21 +0530

    Update RequestLaundry.tsx

--- src/pages/student/RequestLaundry.tsx
+++ src/pages/student/RequestLaundry.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import StudentNavbar from '../../components/StudentNavbar';
-
+import { supabase } from "@/integrations/supabase/client";
 const RequestLaundry = () => {
   const [formData, setFormData] = useState({
     clothesType: 'Normal',
